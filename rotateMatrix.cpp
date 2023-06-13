79afe7728e1945612c0e18fba59fbe33a0d20cbe
Anjali Patel <48998789+purnimapatel@users.noreply.github.com>
2023-06-13 15:14:51 +0530

    Create rotateMatrix.cpp
    Rotate Elements of Matrix Clockwise, submitted.

---
+++ rotateMatrix.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h> 
+
+void rotateMatrix(vector<vector<int>> &mat, int n, int m)
+{
+    // Write your code here
+    int row = 0;
+    int col = 0;
+    int lastRow = n;
+    int lastCol = m;
+    int prev, curr;
+    while(row < lastRow && col < lastCol){
+        if(row+1 == lastRow || col +1 == lastCol){
+            break;
+        }
+
+        prev = mat[row+1][col];
+        for(int i = col; i<lastCol; i++){
+            curr = mat[row][i];
+            mat[row][i] = prev;
+            prev = curr;
+        }
+        row++;
+
+        for(int i = row; i<lastRow; i++){
+            curr = mat[i][lastCol-1];
+            mat[i][lastCol-1] = prev;
+            prev = curr;
+        }
+        lastCol--;
+
+        if(row < lastRow){
+            for(int i = lastCol-1; i >= col; i--){
+                curr = mat[lastRow-1][i];
+                mat[lastRow-1][i] = prev;
+                prev= curr;
+            }
+        }
+        lastRow--;
+        if(col < lastCol){
+            for(int i = lastRow-1; i>=row; i--){
+                curr = mat[i][col];
+                mat[i][col] = prev;
+                prev = curr;
+            }
+        }
+        col++;
+
+    }
+
+}
